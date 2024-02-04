82cb7535f230b47d889df6d1dc55bf3ddf8bfdb1
Dima Fedorenko <dmytro.fedorenko@ut.ee>
2024-02-04 20:48:55 +0200

    Pieces movement

--- main.py
+++ main.py
@@ -33,6 +33,26 @@ def draw_checkers(checker_board):
             if checker_board[y][x] != 0:
                 draw_checker((x*2)+(y%2), y, checker_board[y][x]-1)
 
+def check_tile(selected, checker_board):
+    x, y = selected
+    if (x-(y%2))/2 < 0 or (x-(y%2))/2 != int((x-(y%2))/2):
+        return None
+    return checker_board[y][int((x-(y%2))/2)]
+    
+
+def get_legal_moves(selected, checker_board):
+    x, y = selected
+    legal_moves = []
+    if  y-1 >= 0 and x-1 >= 0 and check_tile((x-1,y-1), checker_board) == 0:
+        legal_moves.append((x-1, y-1))
+    if  y+1 < 8 and x-1 >= 0 and check_tile((x-1,y+1), checker_board) == 0:
+        legal_moves.append((x-1, y+1))
+    if  y-1 >= 0 and x+1 < 8 and check_tile((x+1,y-1), checker_board) == 0:
+        legal_moves.append((x+1, y-1))
+    if  y+1 < 8 and x+1 < 8 and check_tile((x+1,y+1), checker_board) == 0:
+        legal_moves.append((x+1, y+1))
+    return legal_moves
+
 checker_board = [[2, 2, 2, 2],
                  [2, 2, 2, 2],
                  [2, 2, 2, 2],
@@ -45,6 +65,11 @@ checker_board = [[2, 2, 2, 2],
 # Run until the user asks to quit
 running = True
 disp_update = True
+player = 1
+state = "select" # select, move
+selected = (-1, -1)
+legal_moves = []
+
 while running:
 
     for event in pygame.event.get():
@@ -53,7 +78,26 @@ while running:
         if event.type == pygame.MOUSEBUTTONUP:
             pos = pygame.mouse.get_pos()
             x, y = (math.floor(p*8/SCREEN_SIZE) for p in pos)
-            print(x, y)
+            match state:
+                case "select":
+                    if check_tile((x, y), checker_board) == player:
+                        state = "move"
+                        selected = (x, y)
+                        legal_moves = get_legal_moves(selected, checker_board)
+                        print(legal_moves)
+                case "move":
+                    if (x, y) in legal_moves:
+                        checker_board[y][int((x-(y%2))/2)] = checker_board[selected[1]][int((selected[0]-(selected[1]%2))/2)]
+                        checker_board[selected[1]][int((selected[0]-(selected[1]%2))/2)] = 0
+                        player = 1 if player == 2 else 2
+                        disp_update = True
+                        state = "select"
+                        selected = (-1, -1)
+                        legal_moves = []
+                    else:
+                        state = "select"
+                        selected = (-1, -1)
+                        legal_moves = []
 
     draw_board()
     draw_checkers(checker_board)
